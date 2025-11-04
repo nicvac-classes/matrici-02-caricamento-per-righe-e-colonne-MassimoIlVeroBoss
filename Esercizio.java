d9539fedd0c068264f6ed41492e025e9d01c5caf
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2025-11-04 04:56:24 +0000

    consegna

--- Esercizio.java
+++ Esercizio.java
@@ -1,15 +1,61 @@
+import java.util.Scanner;
 
 public class Esercizio {
 
-    public static void riempiPerRighe(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+    public static void riempiPerRighe(int[][] matrix, int rows, int cols) {
+        int rowIndex = 0;
+        int colIndex = 0;
+        int value = 1;
+
+        while (rowIndex < rows) {
+            colIndex = 0;
+            while (colIndex < cols) {
+                matrix[rowIndex][colIndex] = value;
+                ++value;
+                ++colIndex;
+            }
+            ++rowIndex;
+        }
     }
 
-    public static void riempiPerColonne(/* scrivere qui i parametri formali */) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
+    public static void riempiPerColonne(int[][] matrix, int rows, int cols) {
+        int rowIndex = 0;
+        int colIndex = 0;
+        int value = 1;
+
+        while (colIndex < cols) {
+            rowIndex = 0;
+            while (rowIndex < rows) {
+                matrix[rowIndex][colIndex] = value;
+                ++value;
+                ++rowIndex;
+            }
+            ++colIndex;
+        }
     }
 
     public static void main(String[] args) {
-        // SCRIVERE QUI IL CODICE DELL'ESERCIZIO
-    }
+        int rows, cols;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserire il numero di righe della matrice: ");
+        do {
+            rows = Integer.parseInt(scanner.nextLine());
+        } while (rows < 0);
+
+        System.out.println("Inserire il numero di colonne della matrice: ");
+        do {
+            cols = Integer.parseInt(scanner.nextLine());
+        } while (cols < 0);
+
+        int[][] matrix = new int[rows][cols];
+
+        riempiPerRighe(matrix, rows, cols);
+        UtilsMatrice.visualizza(matrix);
+
+        System.out.println();
+
+        riempiPerColonne(matrix, rows, cols);
+        UtilsMatrice.visualizza(matrix);
+    }
 }
